0aaa88ba0117656dd033a39446f2c69a734c0001
Famicet <ammar.arran@gmail.com>
2021-11-04 20:36:55 +0100

    init

---
+++ com.famicet.CoinsJsonGenerator/uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,50 @@
+sap.ui.define(["com/famicet/CoinsJsonGenerator/controller/BaseController", "sap/m/Token"], function (Controller, Token) {
+    "use strict";
+    var servicePointId = 0;
+    var usagePointId = 0;
+
+    return Controller.extend("com.famicet.CoinsJsonGenerator.controller.MainView", {
+        /**
+         * @override
+         */
+        onInit: function() {
+            var oView = this.getView();
+
+            var oServicesPoints = oView.byId("services");
+
+            
+        },
+
+		createNewServicePoint: function(oEvent) {
+			var oView = this.getView();
+            servicePointId++;
+            var input = new sap.m.Input("sPoint" + servicePointId);
+            input.setWidth("700px");
+            input.setRequired(true);
+            oView.byId("servicesPoints").addContent(input);
+		},
+
+		removeServicePoint: function(oEvent) {
+			var oView = this.getView();
+
+            oView.byId("servicesPoints").removeContent(oView.byId("servicesPoints").getContent().length - 1);
+		},
+
+		createNewUsagePoint: function(oEvent) {
+			var oView = this.getView();
+            usagePointId++;
+            var input = new sap.m.Input("uPoint" + usagePointId);
+            input.setWidth("700px");
+            input.setRequired(true);
+            oView.byId("usagesPoints").addContent(input);
+		},
+
+		removeUsagePoint: function(oEvent) {
+			var oView = this.getView();
+
+            oView.byId("usagesPoints").removeContent(oView.byId("usagesPoints").getContent().length - 1);
+		}
+    });
+});
+
+
---
+++ com.famicet.CoinsJsonGenerator/uimodule/webapp/test/integration/arrangements/Startup.js
@@ -0,0 +1,19 @@
+sap.ui.define(["sap/ui/test/Opa5"],
+    /**
+     * @param {typeof sap.ui.test.Opa5} Opa5 
+     */
+    function (Opa5) {
+        "use strict";
+
+        return Opa5.extend("com.famicet.CoinsJsonGenerator.test.integration.arrangements.Startup", {
+            iStartMyApp: function () {
+                this.iStartMyUIComponent({
+                    componentConfig: {
+                        name: "com.famicet.CoinsJsonGenerator",
+                        async: true,
+                        manifest: true
+                    }
+                });
+            }
+        });
+    });
